df8d9baabb6cc8ffb792f6682863e29c8fa7b023
elad237 <tumubu.elad@gmail.com>
2022-10-20 22:43:02 +0100

    updated the style.css file, apps.js file, index.html file and the readme file to add the mobile menu

--- apps.js
+++ apps.js
@@ -1,11 +1,24 @@
 const hamburger = document.querySelector('.bars');
 const overlay = document.querySelector('.open');
 const cancel = document.querySelector('.close');
+const mobileNavItems = document.querySelectorAll('.navlistmoblie > li');
+console.log(mobileNavItems)
 
 hamburger.addEventListener('click', () => {
-    overlay.style.display = 'block';
+  overlay.classList.remove('hidden') 
+  overlay.classList.add('show')
   });
   
   cancel.addEventListener('click', () => {
-    overlay.style.display = 'none';
+    overlay.classList.add('hidden')
+    overlay.classList.remove('show') 
   });
+
+  mobileNavItems.forEach((e) => {
+    e.addEventListener('click', () => {
+    overlay.classList.add('hidden')
+    overlay.classList.remove('show')
+    })
+  })
+
+ 
